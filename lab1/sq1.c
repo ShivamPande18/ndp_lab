cf39ce53a8e5e6856c59dc96b9c4b52114c323f0
Shivam Pande <shivshivampande18@gmail.com>
2025-01-20 14:40:13 +0530

    Update sq1.c

--- lab1/sq1.c
+++ lab1/sq1.c
@@ -1,134 +1,138 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+#define MAXSIZE 50
 
-#define PORT 8080
-#define MAX 1024
+main()
+{
+int sockfd,retval;
+int recedbytes,sentbytes;
+struct sockaddr_in serveraddr;
+int choice,num,res;
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+if(sockfd==-1)
+{
+printf("\nSocket Creation Error");
 
-void sort_array(int arr[], int n, int ascending) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if ((ascending && arr[j] > arr[j + 1]) || (!ascending && arr[j] < arr[j + 1])) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
 }
+printf("%i",sockfd);
+serveraddr.sin_family=AF_INET;
+serveraddr.sin_port=htons(3388);
+serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+retval=connect(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+if(retval==-1)
+{
+printf("Connection error");
 
-void split_odd_even(int arr[], int n) {
-    printf("Odd numbers: ");
+}
+while(1)
+{
+ int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the numbers:\n");
     for (int i = 0; i < n; i++) {
-        if (arr[i] % 2 != 0) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Send the size of the array and the array to the server
+    sentbytes = send(sockfd, &n, sizeof(n), 0);
+    if (sentbytes == -1) {
+        perror("Send error");
+        close(sockfd);
+        exit(1);
+    }
+
+    sentbytes = send(sockfd, arr, n * sizeof(int), 0);
+    if (sentbytes == -1) {
+        perror("Send error");
+        close(sockfd);
+        exit(1);
+    }
+
+    // Now, prompt the user for a choice
+    printf("Enter your choice:\n");
+    printf("1. Search for a number\n");
+    printf("2. Sort numbers\n");
+    printf("3. Exit\n");
+    scanf("%d", &choice);
+
+    // Send choice to the server
+    sentbytes = send(sockfd, &choice, sizeof(choice), 0);
+    if (sentbytes == -1) {
+        perror("Send error");
+        close(sockfd);
+        exit(1);
+    }
+
+    // Handle the search for a number if the choice is 1
+    if (choice == 1) {
+        printf("Enter the number to search for: ");
+        scanf("%d", &num);
+
+        // Send the number to search for
+        sentbytes = send(sockfd, &num, sizeof(num), 0);
+        if (sentbytes == -1) {
+            perror("Send error");
+            close(sockfd);
+            exit(1);
+        }
+
+        // Receive search result (echoed number here)
+        recedbytes = recv(sockfd, &res, sizeof(res), 0);
+        if (recedbytes == -1) {
+            perror("Recv error");
+            close(sockfd);
+            exit(1);
+        }
+	if(res !=-1)
+        printf("Number found at index: %d\n", res);
+        else
+        printf("Number not found");
+    } 
+    // Handle the sorting if the choice is 2
+    else if (choice == 2) {
+        // Receive the sorted array from the server
+        int sort;
+        printf("Enter 8 for sorting in ascending order and 9 for sorting in descending order");
+        scanf("%d",&sort);
+        sentbytes = send(sockfd,&sort,sizeof(sort),0);
+         if (sentbytes == -1) {
+            perror("Send error");
+            close(sockfd);
+            exit(1);
+        }
+        recedbytes = recv(sockfd, arr, sizeof(arr), 0);
+        if (recedbytes == -1) {
+            perror("Recv error");
+            close(sockfd);
+            exit(1);
+        }
+
+        printf("Sorted array:\n");
+        for (int i = 0; i < n; i++) {
             printf("%d ", arr[i]);
         }
+        printf("\n");
+    } else {
+        printf("Invalid choice\n");
     }
-    printf("\nEven numbers: ");
-    for (int i = 0; i < n; i++) {
-        if (arr[i] % 2 == 0) {
-            printf("%d ", arr[i]);
-        }
+    if(choice == 3)
+    {
+    	printf("exiting");
+    	close(sockfd);
+    	exit(1);
     }
-    printf("\n");
-}
-
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[MAX] = {0};
-
-    // Create socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
     }
 
-    // Bind the socket to the PORT
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for incoming connections
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server is listening on port %d", PORT);
-
-    while (1) {
-        // Accept a new connection
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-
-        while (1) {
-            memset(buffer, 0, MAX);
-            read(new_socket, buffer, MAX);
-            if (strcmp(buffer, "exit") == 0) {
-                printf("Client has exited.\n");
-                break;
-            }
-
-            char *token = strtok(buffer, " ");
-            int choice = atoi(token);
-            int arr[100];
-            int n = 0;
-
-            while ((token = strtok(NULL, " ")) != NULL) {
-                arr[n++] = atoi(token);
-            }
-
-            switch (choice) {
-                case 1: // Search
-                    printf("Searching for number %d", arr[0]);
-                    int found = 0;
-                    for (int i = 1; i < n; i++) {
-                        if (arr[i] == arr[0]) {
-                            found = 1;
-                            break;
-                        }
-                    }
-                    if (found) {
-                        send(new_socket, "Number found", strlen("Number found"), 0);
-                    } else {
-                        send(new_socket, "Number not found", strlen("Number not found"), 0);
-                    }
-                    break;
-
-                case 2: // Sort
-                    printf("Sorting array");
-                    sort_array(arr + 1, n - 1, 1); // Sort in ascending order
-                    char sorted[MAX];
-                    sprintf(sorted, "Sorted array: ");
-                    for (int i = 1; i < n; i++) {
-                        sprintf(sorted + strlen(sorted), "%d ", arr[i]);
-                    }
-                    send(new_socket, sorted, strlen(sorted), 0);
-                    break;
-
-                case 3: // Split odd and even
-                    printf("Splitting odd and even numbers");
-                    split_odd_even(arr + 1, n - 1);
-                    send(new_socket, "Odd and even numbers printed on server", strlen("Odd and even numbers printed on server"), 0);
-                    break;
-
-                default:
-                    printf("Invalid choice");
-                    send(new_socket, "Invalid choice", strlen("Invalid choice"), 0);
-                    break;
-            }
-        }
-        close(new_socket);
-    }
+    close(sockfd);
     return 0;
 }
