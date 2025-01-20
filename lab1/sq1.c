95e62cd0488ecb8a993f362316b8b6a4893bed94
Shivam Pande <shivshivampande18@gmail.com>
2025-01-20 15:15:44 +0530

    Update sq1.c

--- lab1/sq1.c
+++ lab1/sq1.c
@@ -1,138 +1,166 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<unistd.h>
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<string.h>
-#include<stdlib.h>
-#define MAXSIZE 50
+#define MAXSIZE 90
 
 main()
 {
-int sockfd,retval;
+int sockfd,newsockfd,retval;
+socklen_t actuallen;
 int recedbytes,sentbytes;
-struct sockaddr_in serveraddr;
-int choice,num,res;
+struct sockaddr_in serveraddr,clientaddr;
+
+int choice,num;
+int i=0;int j=0;
+int a=0;
 sockfd=socket(AF_INET,SOCK_STREAM,0);
+
 if(sockfd==-1)
 {
-printf("\nSocket Creation Error");
-
+printf("\nSocket creation error");
 }
-printf("%i",sockfd);
+
 serveraddr.sin_family=AF_INET;
 serveraddr.sin_port=htons(3388);
-serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-retval=connect(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+serveraddr.sin_addr.s_addr=htons(INADDR_ANY);
+retval=bind(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
 if(retval==-1)
 {
-printf("Connection error");
-
+printf("Binding error");
+close(sockfd);
 }
-while(1)
+
+retval=listen(sockfd,1);
+if(retval==-1)
 {
- int n;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
+close(sockfd);
+}
 
-    int arr[n];
-    printf("Enter the numbers:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+actuallen=sizeof(clientaddr);
+newsockfd=accept(sockfd,(struct sockaddr*)&clientaddr,&actuallen);
 
-    // Send the size of the array and the array to the server
-    sentbytes = send(sockfd, &n, sizeof(n), 0);
-    if (sentbytes == -1) {
-        perror("Send error");
-        close(sockfd);
-        exit(1);
-    }
 
-    sentbytes = send(sockfd, arr, n * sizeof(int), 0);
-    if (sentbytes == -1) {
-        perror("Send error");
-        close(sockfd);
-        exit(1);
-    }
-
-    // Now, prompt the user for a choice
-    printf("Enter your choice:\n");
-    printf("1. Search for a number\n");
-    printf("2. Sort numbers\n");
-    printf("3. Exit\n");
-    scanf("%d", &choice);
-
-    // Send choice to the server
-    sentbytes = send(sockfd, &choice, sizeof(choice), 0);
-    if (sentbytes == -1) {
-        perror("Send error");
-        close(sockfd);
-        exit(1);
-    }
-
-    // Handle the search for a number if the choice is 1
-    if (choice == 1) {
-        printf("Enter the number to search for: ");
-        scanf("%d", &num);
-
-        // Send the number to search for
-        sentbytes = send(sockfd, &num, sizeof(num), 0);
-        if (sentbytes == -1) {
-            perror("Send error");
-            close(sockfd);
-            exit(1);
+if(newsockfd==-1)
+{
+close(sockfd);
+}
+ while (1) {
+        
+        int n;
+        // Receive the array size
+        recedbytes = recv(newsockfd, &n, sizeof(n), 0);
+        if (recedbytes <= 0) {
+            printf("Recv Error\n");
+            break;
         }
 
-        // Receive search result (echoed number here)
-        recedbytes = recv(sockfd, &res, sizeof(res), 0);
-        if (recedbytes == -1) {
-            perror("Recv error");
-            close(sockfd);
-            exit(1);
+        int arr[n];  // Declare array with size 'n'
+
+        // Receive the array
+        recedbytes = recv(newsockfd, arr, n * sizeof(int), 0);
+        if (recedbytes <= 0) {
+            printf("Recv Error\n");
+            break;
         }
-	if(res !=-1)
-        printf("Number found at index: %d\n", res);
-        else
-        printf("Number not found");
-    } 
-    // Handle the sorting if the choice is 2
-    else if (choice == 2) {
-        // Receive the sorted array from the server
-        int sort;
-        printf("Enter 8 for sorting in ascending order and 9 for sorting in descending order");
-        scanf("%d",&sort);
-        sentbytes = send(sockfd,&sort,sizeof(sort),0);
-         if (sentbytes == -1) {
-            perror("Send error");
-            close(sockfd);
-            exit(1);
-        }
-        recedbytes = recv(sockfd, arr, sizeof(arr), 0);
-        if (recedbytes == -1) {
-            perror("Recv error");
-            close(sockfd);
-            exit(1);
+        // Receive the choice
+        recedbytes = recv(newsockfd, &choice, sizeof(choice), 0);
+        if (recedbytes <= 0) {
+            printf("Recv Error\n");
+            break;
         }
 
-        printf("Sorted array:\n");
-        for (int i = 0; i < n; i++) {
-            printf("%d ", arr[i]);
+
+        if (choice == 1) {
+            // Search for a number
+            printf("Client chose to search for a number\n");
+            // Receive the number to search for
+            recedbytes = recv(newsockfd, &num, sizeof(num), 0);
+            if (recedbytes <= 0) {
+                printf("Recv error\n");
+                break;
+            }
+            printf("Searching for number: %d\n", num);
+            int res = -1;  // Default result if not found
+            for (i = 0; i < n; i++) {
+                if (arr[i] == num) {
+                    res = i;
+                    break;
+                }
+            }
+            // Send result back to the client (index of the number)
+            sentbytes = send(newsockfd, &res, sizeof(res), 0);
+            if (sentbytes == -1) {
+                printf("Send error\n");
+                break;
+            }
         }
-        printf("\n");
-    } else {
-        printf("Invalid choice\n");
-    }
-    if(choice == 3)
-    {
-    	printf("exiting");
-    	close(sockfd);
-    	exit(1);
-    }
+        else if (choice == 2) {
+            // Sorting numbers
+            int sort;
+            // Receive the sort choice (ascending or descending)
+            recedbytes = recv(newsockfd, &sort, sizeof(sort), 0);
+            if (recedbytes <= 0) {
+                printf("Recv Error\n");
+                break;
+            }
+
+            if (sort == 8) {
+                // Sorting in ascending order
+                printf("Sorting in ascending order\n");
+                for (i = 0; i < n - 1; i++) {
+                    for (j = 0; j < n - 1 - i; j++) {
+                        if (arr[j] > arr[j + 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                }
+                sentbytes = send(newsockfd, arr, n * sizeof(int), 0);
+                if (sentbytes == -1) {
+                    printf("Send error\n");
+                    break;
+                }
+            }
+            else if (sort == 9) {
+                // Sorting in descending order
+                printf("Sorting in descending order\n");
+                for (i = 0; i < n - 1; i++) {
+                    for (j = 0; j < n - 1 - i; j++) {
+                        if (arr[j] < arr[j + 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                }
+                sentbytes = send(newsockfd, arr, n * sizeof(int), 0);
+                if (sentbytes == -1) {
+                    printf("Send error\n");
+                    break;
+                }
+            }
+            else {
+                printf("Invalid sort choice\n");
+            }
+        }
+        else if (choice == 3) {
+            // Exit condition
+            break;
+        }
+        else {
+            printf("Invalid choice\n");
+            break;
+        }
     }
 
+    // Close the sockets outside the loop to allow proper connection handling
+    close(newsockfd);
     close(sockfd);
+
     return 0;
 }
